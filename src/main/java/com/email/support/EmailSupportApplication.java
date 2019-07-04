c892b2af50c6c060b3159c3c22f718b20c39f443
隋国珍 <s15863549041@126.com>
2019-07-04 18:21:39 +0800

    邮件系统

---
+++ src/main/java/com/email/support/EmailSupportApplication.java
@@ -0,0 +1,13 @@
+package com.email.support;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailSupportApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmailSupportApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/email/support/EmailSupportApplicationTests.java
@@ -0,0 +1,16 @@
+package com.email.support;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EmailSupportApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
